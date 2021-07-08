a1b0280c13c90d9496e61235267d0a65050750f1
Micah Tanner <micahsoftdotexe@gmail.com>
2021-07-07 20:33:45 -0400

    Added Local Database

---
+++ config/db-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=workorder',
+    'username' => 'micaht',
+    'password' => 'conn123',
+    'charset' => 'utf8',
+];
--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-
-return [
+$db = [
+    'charset' => 'utf8',
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=workorder',
+    'dsn' => 'mysql:host=localhost;dbname=workorder',
     'username' => 'micaht',
     'password' => 'conn123',
     'charset' => 'utf8',
@@ -12,3 +12,9 @@ return [
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
+if (file_exists(__DIR__ . '/db-local.php')) {
+    $db = array_merge($db, require(__DIR__ . '/db-local.php'));
+}
+
+return $db;
