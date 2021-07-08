39b0995b951f8f689e63e20fcd3933e492fd7491
Micah Tanner <micahsoftdotexe@gmail.com>
2021-07-07 20:53:05 -0400

    gitignore stuff

--- config/db-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'charset' => 'utf8',
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=workorder',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
