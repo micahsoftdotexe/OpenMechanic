7fdbde20ddfe89f07365964fa8d4d5e5f39d616f
Micah Tanner <micahsoftdotexe@gmail.com>
2021-07-07 20:47:44 -0400

    Another Database Fix

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $db = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=workorder',
     'username' => 'root',
-    'password' => 'conn123',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
