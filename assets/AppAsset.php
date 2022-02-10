6f6d44f1d97e90a731fa26f6d613fac09f15d162
Micah Tanner <micahrocz777@gmail.com>
2022-02-10 07:00:54 -0500

    Added new style and font

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,6 +20,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/siteNew.css',
         'css/site.css',
         //'css/bootstrap.min.css'
     ];
