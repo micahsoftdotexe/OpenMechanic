9e0f8abba0730d88aac3ad8e53a628dfc27f8acc
Micah Tanner <micahrocz777@gmail.com>
2022-02-15 14:28:46 -0500

    Made notes bigger

--- views/workorder/_form_notes.php
+++ views/workorder/_form_notes.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
         'action' => ['workorder/update-notes', 'id' => $model->workorder_id],
         'id' => 'note-form' 
     ]) ?>
-    <?= $noteForm->field($model, 'note')->label(Yii::t('app', 'Notes'))->textArea(); ?>
+    <?= $noteForm->field($model, 'note')->label(Yii::t('app', 'Notes'))->textArea(['rows'=>25]); ?>
     <?= $noteForm->field($model, 'workorder_id')->hiddenInput(['value' => $model->workorder_id])->label(false) ?>
     <?= Html::submitButton('<span class="fa fa-upload" aria-hidden="true"></span> ' . Yii::t('app', 'Save'), [
         'class'             => 'btn btn-success',
