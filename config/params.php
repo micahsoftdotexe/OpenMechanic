628a847734cae1f240f1182dff39ee1ae66581de
Micah Tanner <micahrocz777@gmail.com>
2022-03-24 12:51:59 -0400

    Styling params

--- config/params.php
+++ config/params.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail'      => 'admin@example.com',
     'applicationName' => 'OpenMechanic',
-    'companyName' => 'Company Name',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'sales_tax' => 0.07,
+    'companyName'     => 'Company Name',
+    'senderEmail'     => 'noreply@example.com',
+    'senderName'      => 'Example.com mailer',
+    'sales_tax'       => 0.07,
 ];
